a398c4c8c489804219407872aa53a50ead02fafd
Javier <villayali2001@gmail.com>
2023-04-17 21:55:01 -0500

    Confirmo

--- Tarea.c
+++ Tarea.c
@@ -4,9 +4,9 @@ int main(){
         int i, j, k;
 
     for (i = 1; i <= 9; i++) {
-        j = (i - 1) / 3 + 1; // cálculo de la primera componente de la terna
-        k = i % 3;           // cálculo de la segunda componente de la terna
-        if (k == 0) {        // ajuste para cuando k es igual a cero
+        j = (i - 1) / 3 + 1; 
+        k = i % 3;           
+        if (k == 0) {        
             k = 3;
         }
         printf("%d %d %d\n", i, j, k);
